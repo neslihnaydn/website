e251c613efb3e8c63f30def39e7b8c46c95ba5c5
neslihanaydin <neslihnaydn@gmail.com>
2021-04-21 22:27:04 +0200

    social media links etc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: `./home.component.html`,
-  styles: [`
-    .hero {
-      background-image: url('/assets/img/background2.jpeg') !important;
-      background-size: conver;
-      background-position: center center;
-    }
-  `]
+  styles: []
 })
 export class HomeComponent implements OnInit {
 
